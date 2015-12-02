327588f8245ce710239e9bbf269cb24b1b0a8006
wlsdud <wlsdud5938@naver.com>
2015-12-02 22:33:05 +0900

    d

--- NewScheduler/src/kmu/tp/newscheduler/MainActivity.java
+++ NewScheduler/src/kmu/tp/newscheduler/MainActivity.java
@@ -1,9 +1,20 @@
 package kmu.tp.newscheduler;
 
 import android.app.Activity;
-import android.os.Bundle;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
+import android.widget.*;
+import android.widget.Button;
+import android.widget.FrameLayout.LayoutParams;
+import android.view.*;
+import android.app.Activity;
+import android.app.*;
+import android.os.*;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View.OnClickListener;
 
 public class MainActivity extends Activity {
 
@@ -11,7 +22,66 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setTheme(android.R.style.Theme_NoTitleBar_Fullscreen);
+
+		
+		final DBManager dbManager = new DBManager(getApplicationContext(),"schedule.db",null,1);
+		SQLiteDatabase db = dbManager.getReadableDatabase();
+		//스케줄 목록 호출
+		Cursor scheduleLists = db.rawQuery("select * from schedule order by favorite desc, no desc",null);
+		RelativeLayout container = (RelativeLayout) findViewById(R.id.scheduleListView);
+		
+		//목록이 있는지 확인한다.
+		if(scheduleLists.getCount() == 0) 
+		{	
+			//TextView 생성
+			TextView noSchedule = new TextView(this);
+			noSchedule.setTextSize(14);
+			noSchedule.setTextColor(Color.BLACK);
+			noSchedule.setGravity(Gravity.CENTER_HORIZONTAL);
+			noSchedule.setText("일정이 없습니다. 지금 일정을 추가해 보세요!");
+			
+			container.addView(noSchedule);
+		}
+		else
+		{
+			for(boolean Lists = scheduleLists.moveToFirst(); Lists; Lists=scheduleLists.moveToNext())
+			{
+				String Subject = scheduleLists.getString(1);
+				String StartDate = scheduleLists.getString(2);
+				String EndDate = scheduleLists.getString(3);
+				int isFavorited = scheduleLists.getInt(5);	//0 : Off, 1 : On
+				
+				TextView Schedules = new TextView(this);
+				Schedules.setTextSize(16);
+				Schedules.setTextColor(Color.BLACK);
+				Schedules.setGravity(Gravity.CENTER_HORIZONTAL);
+				//시작일과 종료일, 그리고 제목만 간단하게 표시한다.
+				Schedules.setText("제목 : "+Subject+"\n시작일 : "+StartDate+"\n종료일 : "+EndDate);
+			}
+		}
+		
+		
+		Button addMenuBtn = (Button) findViewById(R.id.addSchedule);
+		addMenuBtn.setOnClickListener(new OnClickListener(){
+			public void onClick(View v)
+			{
+				Intent intent = new Intent(MainActivity.this, AddSchedule.class);
+				MainActivity.this.startActivity(intent);
+			}
+		});
+		Button delMenuBtn = (Button) findViewById(R.id.delSchedule);
+		delMenuBtn.setOnClickListener(new OnClickListener(){
+			public void onClick(View v)
+			{
+				//일정 삭제 View Load
+			}
+		});
 	}
+	
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -31,4 +101,6 @@ public class MainActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+	
+	
 }
